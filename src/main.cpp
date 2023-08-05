333c30c17c0ea394053ecf94f4bc658040f5ea4a
Brochu <alexbrochu1@gmail.com>
2023-08-05 12:15:13 -0400

    [SETUP] Include and use day 1 for first initial test

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <stdio.h>
+
 #include "parsing.h"
+#include "day1.h"
 
 int main(int argc, char **argv) {
     printf("[MAIN] Solutions for day ...\n");
 
     int val = Parse::tests();
     printf("[MAIN] Here's a value = %i\n", val);
+
+    int solved = Solution::part_1();
+    printf("[MAIN] Here the part_1 solution = %i\n", solved);
 
     return 0;
 }
