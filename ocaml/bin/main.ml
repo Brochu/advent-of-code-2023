9861efcafb4dbcbf549dd36bcad6ff1865b94dd2
Alex <alexbrochu1@gmail.com>
2023-09-09 17:56:25 -0400

    [SETUP] One general lib, solutions specific lib, both working

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -1,4 +1,10 @@
-open Ocaml.Day1
 open Core
+open General.Helpers
 
-let () = print_endline (string_of_int day1)
+open Solutions.Day1
+open Solutions.Day2
+
+let () =
+    print_endline (string_of_int x);
+    print_endline (string_of_int day1);
+    print_endline (string_of_int day2);
