653773b4f62e6c0e821c7ab8d3abbd2b4204aa0a
Brochu <alexbrochu1@gmail.com>
2023-11-03 13:46:31 -0400

    [CLEAN] remove example input read code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <stdio.h>
 
 #if   DAY == 1
@@ -9,14 +8,6 @@
 #include "parsing.h"
 
 int main(int argc, char **argv) {
-    // This is how we will read input in this project
-    //printf("-----------\n");
-    //std::string line = "Hello!";
-    //while (std::getline(std::cin, line)) {
-    //    printf("%s\n", line.c_str());
-    //}
-    //printf("-----------\n");
-
     printf("[MAIN] Solutions for day %i\n", DAY);
     printf("[MAIN] Here the part_1 solution = %s\n", Solution::part_1().c_str());
     printf("[MAIN] Here the part_2 solution = %s\n", Solution::part_2().c_str());
