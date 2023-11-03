e836094836a63dfdb024365b74ca0de06f29fbd4
Brochu <alexbrochu1@gmail.com>
2023-11-03 13:55:12 -0400

    [DEV] Day 1 part 1 example test

--- src/day1.cpp
+++ src/day1.cpp
@@ -1,9 +1,25 @@
 #include "day1.h"
 
+#include <iostream>
+
 namespace Solution {
 
 std::string part_1() {
-    return "69";
+    int value = 0;
+    std::string line = "";
+    while (std::getline(std::cin, line)) {
+        char sign = line[0];
+        int v = atoi(line.substr(1).c_str());
+
+        if (sign == '+') {
+            value += v;
+        }
+        else if (sign == '-') {
+            value -= v;
+        }
+    }
+
+    return std::to_string(value);
 }
 
 std::string part_2() {
