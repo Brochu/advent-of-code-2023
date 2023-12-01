2ffe13fd21a036e1e0c57278c9e26a871a70e4a5
Brochu <alexbrochu1@gmail.com>
2023-12-01 00:16:33 -0500

    [DEV] Can send the separator for enum_str

--- include/parsing.h
+++ include/parsing.h
@@ -10,14 +10,14 @@ std::string read_file(const std::string &path);
 std::vector<char*> split_str(std::string &&str);
 
 template<typename Fn>
-void enum_str(std::string &&str, Fn func) {
+void enum_str(std::string &&str, const std::string &separator, Fn func) {
     std::vector<char*> tokens;
     char *token, *nextt = nullptr;
 
-    token = strtok_s(str.data(), "\n", &nextt);
+    token = strtok_s(str.data(), separator.c_str(), &nextt);
     while(token) {
         func(token);
-        token = strtok_s(NULL, "\n", &nextt);
+        token = strtok_s(NULL, separator.c_str(), &nextt);
     }
 }
 
