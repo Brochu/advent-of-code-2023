820f10af0ebf193215891e90efea5119c87b54fe
Brochu <alexbrochu1@gmail.com>
2023-11-30 21:13:59 -0500

    [SETUP] Added template day code file

---
+++ src/day0.cpp
@@ -0,0 +1,23 @@
+#include "day.h"
+#include "parsing.h"
+
+namespace Solution {
+
+std::string part1() {
+    return {};
+}
+
+std::string part2() {
+    return {};
+}
+
+int run(std::string *part1_out, std::string *part2_out) {
+    std::string input = INCLUDE_STR(".\\inputs\\day0.txt");
+
+    *part1_out = part1();
+    *part2_out = part2();
+
+    return 0;
+}
+
+}
