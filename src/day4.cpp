e0ef3ac471c16635f0ee960b2d54d5e850e68404
Brochu <alexbrochu1@gmail.com>
2023-12-03 05:36:55 -0500

    [DEV] Day4: Setting up for day 4 tomorrow

---
+++ src/day4.cpp
@@ -0,0 +1,24 @@
+#include "day.h"
+#include "parsing.h"
+
+namespace Solution {
+
+std::string part1() {
+    return "NotCompleted";
+}
+
+std::string part2() {
+    return "NotCompleted";
+}
+
+int run(std::string *part1_out, std::string *part2_out) {
+    std::string in = INCLUDE_STR(".\\inputs\\day4_demo1.txt");
+    printf("%s\n", in.c_str());
+
+    *part1_out = part1();
+    *part2_out = part2();
+
+    return 0;
+}
+
+}
