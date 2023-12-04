280622f5bb7243d5306b32b13646fa8321555fe9
Brochu <alexbrochu1@gmail.com>
2023-12-04 10:50:01 -0500

    [DEV] Day5: Setting up for today's puzzle

---
+++ src/day5.cpp
@@ -0,0 +1,23 @@
+#include "day.h"
+#include "parsing.h"
+
+namespace Solution {
+
+std::string part1() {
+    return "NotCompleted";
+}
+
+std::string part2() {
+    return "NotCompleted";
+}
+
+int run(std::string *part1_out, std::string *part2_out) {
+    std::string in = INCLUDE_STR(".\\inputs\\day0.txt");
+
+    *part1_out = part1();
+    *part2_out = part2();
+
+    return 0;
+}
+
+}
