fb5e2f1ebd1ce41dffa642723f3e4b6721c7538b
Brochu <alexbrochu1@gmail.com>
2023-12-04 03:12:08 -0500

    [DEV] Day4: Completed part 2!!

--- src/day4.cpp
+++ src/day4.cpp
@@ -5,45 +5,57 @@
 
 namespace Solution {
 
-#define WIN_COUNT 5
-#define WIN_FORMAT "%i %i %i %i %i"
-#define WIN_EXPAND(a) a[0], a[1], a[2], a[3], a[4]
-#define PICK_COUNT 8
-#define PICK_FORMAT "%i %i %i %i %i %i %i %i"
-#define PICK_EXPAND(a) a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]
-//#define WIN_COUNT 10
-//#define WIN_FORMAT "%i %i %i %i %i %i %i %i %i %i"
-//#define WIN_EXPAND(a) a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9]
-//#define PICK_COUNT 25
-//#define PICK_FORMAT "%i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i"
-//#define PICK_EXPAND(a) a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15], a[16], a[17], a[18], a[19], a[20], a[21], a[22], a[23], a[24]
+//#define WIN_COUNT 5
+//#define WIN_FORMAT "%i %i %i %i %i"
+//#define WIN_EXPAND(a) a[0], a[1], a[2], a[3], a[4]
+//#define PICK_COUNT 8
+//#define PICK_FORMAT "%i %i %i %i %i %i %i %i"
+//#define PICK_EXPAND(a) a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]
+#define WIN_COUNT 10
+#define WIN_FORMAT "%i %i %i %i %i %i %i %i %i %i"
+#define WIN_EXPAND(a) a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9]
+#define PICK_COUNT 25
+#define PICK_FORMAT "%i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i %i"
+#define PICK_EXPAND(a) a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15], a[16], a[17], a[18], a[19], a[20], a[21], a[22], a[23], a[24]
 
 struct Card {
     int id;
     int winNums[WIN_COUNT];
     int pickNums[PICK_COUNT];
+    int score = 0;
 };
 
 std::string part1(std::span<Card> cards) {
     size_t total = 0;
 
     for (Card &card : cards) {
-        size_t count = 0;
-
-        for (int pick : card.pickNums) {
-            if (std::find(std::begin(card.winNums), std::end(card.winNums), pick) != std::end(card.winNums)) {
-                count ++;
-            }
-        }
-        if (count > 0) {
-            total += pow(2, count - 1);
+        if (card.score > 0) {
+            total += pow(2, card.score - 1);
         }
     }
     return std::to_string(total);
 }
 
 std::string part2(std::span<Card> cards) {
-    return "NotCompleted";
+    int *counts = new int[cards.size()];
+    // Init
+    for (int i = 0; i < cards.size(); i++) {
+        counts[i] = 1;
+    }
+
+    for (int i = 0; i < cards.size(); i++) {
+        Card card = cards[i];
+        for (int j = 1; j <= card.score; j++) {
+            counts[i + j] += counts[i];
+        }
+    }
+
+    size_t total = 0;
+    for (int i = 0; i < cards.size(); i++) {
+        total += counts[i];
+    }
+    delete[] counts;
+    return std::to_string(total);
 }
 
 Card parse_card(char *line) {
@@ -53,12 +65,18 @@ Card parse_card(char *line) {
     std::vector<char*> nums = Parse::split_char(strstr(line, ": ") + 2, " | ");
     sscanf_s(nums[0], WIN_FORMAT, WIN_EXPAND(&card.winNums));
     sscanf_s(nums[1], PICK_FORMAT, PICK_EXPAND(&card.pickNums));
+
+    for (int pick : card.pickNums) {
+        if (std::find(std::begin(card.winNums), std::end(card.winNums), pick) != std::end(card.winNums)) {
+            card.score++;
+        }
+    }
     return card;
 }
 
 int run(std::string *part1_out, std::string *part2_out) {
-    std::string in = INCLUDE_STR(".\\inputs\\day4_demo1.txt");
-    //std::string in = INCLUDE_STR(".\\inputs\\day4.txt");
+    //std::string in = INCLUDE_STR(".\\inputs\\day4_demo1.txt");
+    std::string in = INCLUDE_STR(".\\inputs\\day4.txt");
     std::vector<Card> cards;
 
     Parse::enum_str(std::move(in), "\n", [&cards](char *token) {
