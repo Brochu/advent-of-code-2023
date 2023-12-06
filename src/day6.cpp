725e422f6f62a02dd9da4522b971b392e1de3625
Brochu <alexbrochu1@gmail.com>
2023-12-06 18:43:32 -0500

    [DEV] Day6: Done with part 1

--- src/day6.cpp
+++ src/day6.cpp
@@ -1,13 +1,46 @@
 #include "day.h"
 #include "parsing.h"
 
+#include <span>
+
 namespace Solution {
 
-#define FILE_PATH ".\\inputs\\day6_demo1.txt"
-//#define FILE_PATH ".\\inputs\\day6.txt"
+//#define FILE_PATH ".\\inputs\\day6_demo1.txt"
+//#define RACE_COUNT 3
+//#define RACE_FORMAT "%i %i %i"
+//#define RACE_EXPAND(a) &a[0], &a[1], &a[2]
+#define FILE_PATH ".\\inputs\\day6.txt"
+#define RACE_COUNT 4
+#define RACE_FORMAT "%i %i %i %i"
+#define RACE_EXPAND(a) &a[0], &a[1], &a[2], &a[3]
 
-std::string part1() {
-    return "NotCompleted";
+struct Race {
+    int time;
+    int dist;
+};
+void debug_race(std::span<Race> races) {
+    for (Race &r : races) {
+        printf("[RACE] t = %i; d = %i\n", r.time, r.dist);
+    }
+}
+
+std::string part1(std::span<Race> races) {
+    int margin = 1;
+
+    for (Race &r : races) {
+        int count = 0;
+        for (int i = 1; i < r.time; i++) {
+            int val = i * (r.time - i);
+            if (val > r.dist) { count++; }
+
+            //debug_race({&r ,1});
+            //printf("start %i, speed = %i, remain = %i, max = %i\n", i, i, (r.time - i), val);
+        }
+        margin *= count;
+        //printf("count = %i, margin = %i\n--------------\n", count, margin);
+    }
+
+    return std::to_string(margin);
 }
 
 std::string part2() {
@@ -16,9 +49,20 @@ std::string part2() {
 
 int run(std::string *part1_out, std::string *part2_out) {
     std::string in = INCLUDE_STR(FILE_PATH);
-    printf("%s\n", in.c_str());
+    std::vector<char*> lines = Parse::split_str(std::move(in), "\n");
 
-    *part1_out = part1();
+    int times[RACE_COUNT];
+    sscanf_s(Parse::split_char(lines[0], ": ")[1], RACE_FORMAT, RACE_EXPAND(times));
+    int distances[RACE_COUNT];
+    sscanf_s(Parse::split_char(lines[1], ": ")[1], RACE_FORMAT, RACE_EXPAND(distances));
+
+    Race data[RACE_COUNT];
+    for (int i = 0; i < RACE_COUNT; i++) {
+        data[i].time = times[i];
+        data[i].dist = distances[i];
+    }
+
+    *part1_out = part1(data);
     *part2_out = part2();
 
     return 0;
