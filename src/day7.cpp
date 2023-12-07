d1bcb6a152950cef8819432755d8c159fff3265c
Brochu <alexbrochu1@gmail.com>
2023-12-07 01:03:55 -0500

    [DEV] Day7: Progress on part one, the hands are sorted

--- src/day7.cpp
+++ src/day7.cpp
@@ -1,6 +1,7 @@
 #include "day.h"
 #include "parsing.h"
 
+#include <algorithm>
 #include <span>
 
 namespace Solution {
@@ -11,8 +12,37 @@ namespace Solution {
 #else // ------------------------------------
 #define FILE_PATH ".\\inputs\\day7.txt"
 #endif // ------------------------------------
+#define HAND_COUNT 5
+#define HAND_FORMAT "%c%c%c%c%c %i"
+#define HAND_EXPAND(c, b) &c[0], 1, &c[1], 1, &c[2], 1, &c[3], 1, &c[4], 1, &b
+#define FIND(span, val) std::find(std::begin(span), std::end(span), val) != std::end(span)
 
-std::string part1() {
+enum Type { HighCard, OnePair, TwoPairs, ThreeOfAKind, FullHouse, FourOfAKind, FiveOfAKind, };
+
+struct Hand {
+    char cards[HAND_COUNT];
+    int bet;
+    Type type;
+};
+void debug_hands(std::span<Hand> hands) {
+    for (Hand &hand : hands) {
+        printf("(%i)[%i] %c%c%c%c%c\n", hand.type, hand.bet, hand.cards[0], hand.cards[1], hand.cards[2], hand.cards[3], hand.cards[4]);
+    }
+}
+
+std::string part1(std::vector<Hand> hands) {
+    auto pred = [](const Hand &left, const Hand &right) {
+        if (left.type == right.type) {
+            int idx = 0;
+            while (left.cards[idx] == right.cards[idx]) { idx++; }
+            return left.cards[idx] > right.cards[idx];
+        }
+        return left.type < right.type;
+    };
+    std::sort(hands.begin(), hands.end(), pred);
+
+    debug_hands(hands);
+
     return "NotCompleted";
 }
 
@@ -20,11 +50,82 @@ std::string part2() {
     return "NotCompleted";
 }
 
+int card_value(char card) {
+    if (card == '2') return 2;
+    else if (card == '3') return 3;
+    else if (card == '4') return 4;
+    else if (card == '5') return 5;
+    else if (card == '6') return 6;
+    else if (card == '7') return 7;
+    else if (card == '8') return 8;
+    else if (card == '9') return 9;
+    else if (card == 'T') return 10;
+    else if (card == 'J') return 11;
+    else if (card == 'Q') return 12;
+    else if (card == 'K') return 13;
+    else if (card == 'A') return 14;
+    else return -1;
+}
+
+Type find_type(std::span<char> cards) {
+    int counts[15] { 0 };
+    for (char c : cards) {
+        counts[card_value(c)]++;
+    }
+
+    for (char c : cards) {
+        printf("%c ", c);
+    }
+    printf(" -> ");
+    for (int i = card_value('2'); i <= card_value('A'); i++) {
+        printf("%i ", counts[i]);
+    }
+    printf("\n");
+
+    if (FIND(counts, 5)) {
+        return Type::FiveOfAKind;
+    } else if (FIND(counts, 4)) {
+        return Type::FourOfAKind;
+    } else if (FIND(counts, 3)) {
+        if (FIND(counts, 2)) {
+            return Type::FullHouse;
+        } else {
+            return Type::ThreeOfAKind;
+        }
+    } else if (FIND(counts, 2)) {
+        int pairCount = 0;
+        for (int i = card_value('2'); i <= card_value('A'); i++) {
+            if (counts[i] == 2) pairCount++;
+        }
+        if (pairCount == 2) {
+            return Type::TwoPairs;
+        } else {
+            return Type::OnePair;
+        }
+    }
+    return Type::HighCard;
+};
+
+std::vector<Hand> parse_hands(std::vector<char*> data) {
+    std::vector<Hand> hands;
+
+    for (char *entry : data) {
+        Hand h { 0 };
+        sscanf_s(entry, HAND_FORMAT, HAND_EXPAND(h.cards, h.bet));
+        h.type = find_type(h.cards);
+        hands.push_back(h);
+    }
+
+    return hands;
+}
+
 int run(std::string *part1_out, std::string *part2_out) {
     std::string in = INCLUDE_STR(FILE_PATH);
-    printf("%s\n", in.c_str());
+    std::vector<char*> data = Parse::split_str(std::move(in), "\n");
 
-    *part1_out = part1();
+    std::vector<Hand> hands = parse_hands(data);
+
+    *part1_out = part1(hands);
     *part2_out = part2();
 
     return 0;
