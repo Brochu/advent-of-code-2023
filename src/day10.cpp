04353f8ceff38387c7daeaafb6bc97591f2ed687
Brochu <alexbrochu1@gmail.com>
2023-12-10 02:46:08 -0500

    [DEV] Day10: Progress, parsing done, we have the paths to explore

--- src/day10.cpp
+++ src/day10.cpp
@@ -8,57 +8,82 @@ namespace Solution {
 #define DEMO 1
 #if DEMO == 1 // ------------------------------------
 #define FILE_PATH ".\\inputs\\day10_demo1.txt"
+#define GRID_SIZE 5
 #else // ------------------------------------
 #define FILE_PATH ".\\inputs\\day10.txt"
+#define GRID_SIZE 140
 #endif // ------------------------------------
 
-struct Coord {
-    u8 x, y;
-};
+enum Dir { N, W, E, S };
 
 struct Grid {
-    std::vector<char*> cells;
-    Coord start;
+    char *cells;
+    usize start;
 };
-
-void debug_grid(Grid grid) {
-    for (int i = 0; i < grid.cells.size(); i++) {
-        for (int j = 0; j < strlen(grid.cells[i]); j++) {
-            printf("%c ", grid.cells[i][j]);
+void debug_grid(Grid &g) {
+    printf("[GRID]\n");
+    for (int i = 0; i < GRID_SIZE; i++) {
+        for (int j = 0; j < GRID_SIZE; j++) {
+            printf("%c", g.cells[j + (GRID_SIZE * i)]);
         }
         printf("\n");
     }
-    printf("[START] (%i, %i)\n", grid.start.x, grid.start.y);
+    printf("\n");
 }
 
-std::string part1(Grid &grid) {
-    return "NotCompleted";
-}
-
-std::string part2(Grid &grid) {
-    return "NotCompleted";
-}
-
-void find_start(Grid &grid) {
-    for (int i = 0; i < grid.cells.size(); i++) {
-        for (int j = 0; j < strlen(grid.cells[i]); j++) {
-            if (grid.cells[i][j] == 'S') {
-                grid.start = { (u8)i, (u8)j };
-                return;
-            }
-        }
+struct Path {
+    char *pos;
+    Dir from;
+    usize len;
+};
+void debug_paths(std::span<Path> paths) {
+    for (Path &p : paths) {
+        char d;
+        if (p.from == N) d = 'N';
+        else if (p.from == W) d = 'W';
+        else if (p.from == E) d = 'E';
+        else if (p.from == S) d = 'S';
+        printf("[PATH] %c from %c [len=%lld]\n", *p.pos, d, p.len);
     }
+}
+
+std::string part1(Grid &g) {
+    debug_grid(g);
+    std::vector<Path> paths;
+    char up = g.cells[g.start - GRID_SIZE];
+    if (up == '|' || up == '7' || up == 'F') { paths.push_back({ &up, Dir::S, 1 }); }
+    char lf = g.cells[g.start - 1];
+    if (lf == '-' || lf == 'L' || lf == 'F') { paths.push_back({ &lf, Dir::E, 1 }); }
+    char rt = g.cells[g.start + 1];
+    if (rt == '-' || rt == 'J' || rt == '7') { paths.push_back({ &rt, Dir::W, 1 }); }
+    char dw = g.cells[g.start + GRID_SIZE];
+    if (dw == '|' || dw == 'J' || dw == 'L') { paths.push_back({ &dw, Dir::N, 1 }); }
+
+    debug_paths(paths);
+    return "NotCompleted";
+}
+
+std::string part2(Grid &g) {
+    return "NotCompleted";
 }
 
 int run(std::string *part1_out, std::string *part2_out) {
     std::string in = INCLUDE_STR(FILE_PATH);
-    std::vector<char*> lines = Parse::split_str(std::move(in), "\n");
+    std::string gridStr;
+    Parse::enum_str(std::move(in), "\n", [&gridStr](char *token){
+        gridStr.append(token);
+    });
 
-    Grid grid = { lines };
-    find_start(grid);
+    Grid g;
+    for (usize i = 0; i < gridStr.size(); i++) {
+        if (gridStr[i] == 'S') {
+            g.cells = gridStr.data();
+            g.start = i;
+        }
+    }
 
-    *part1_out = part1(grid);
-    *part2_out = part2(grid);
+    *part1_out = part1(g);
+    *part2_out = part2(g);
 
     return 0;
 }
