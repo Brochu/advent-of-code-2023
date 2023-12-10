c09c24dbb68c8067c818c98c30551b84f6a635dc
Brochu <alexbrochu1@gmail.com>
2023-12-10 00:45:19 -0500

    [DEV] Day10: finished with parsing

--- src/day10.cpp
+++ src/day10.cpp
@@ -12,20 +12,53 @@ namespace Solution {
 #define FILE_PATH ".\\inputs\\day10.txt"
 #endif // ------------------------------------
 
-std::string part1() {
+struct Coord {
+    u8 x, y;
+};
+
+struct Grid {
+    std::vector<char*> cells;
+    Coord start;
+};
+
+void debug_grid(Grid grid) {
+    for (int i = 0; i < grid.cells.size(); i++) {
+        for (int j = 0; j < strlen(grid.cells[i]); j++) {
+            printf("%c ", grid.cells[i][j]);
+        }
+        printf("\n");
+    }
+    printf("[START] (%i, %i)\n", grid.start.x, grid.start.y);
+}
+
+std::string part1(Grid &grid) {
     return "NotCompleted";
 }
 
-std::string part2() {
+std::string part2(Grid &grid) {
     return "NotCompleted";
+}
+
+void find_start(Grid &grid) {
+    for (int i = 0; i < grid.cells.size(); i++) {
+        for (int j = 0; j < strlen(grid.cells[i]); j++) {
+            if (grid.cells[i][j] == 'S') {
+                grid.start = { (u8)i, (u8)j };
+                return;
+            }
+        }
+    }
 }
 
 int run(std::string *part1_out, std::string *part2_out) {
     std::string in = INCLUDE_STR(FILE_PATH);
-    printf("%s\n", in.c_str());
+    std::vector<char*> lines = Parse::split_str(std::move(in), "\n");
 
-    *part1_out = part1();
-    *part2_out = part2();
+    Grid grid = { lines };
+    find_start(grid);
+
+    *part1_out = part1(grid);
+    *part2_out = part2(grid);
 
     return 0;
 }
