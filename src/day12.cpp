a047e69d5db8520011a3881d2c3af8ff04cc39ca
Brochu <alexbrochu1@gmail.com>
2023-12-12 00:41:26 -0500

    [DEV] Day12: Got the parsing done, started to work on algo for part 1

--- src/day12.cpp
+++ src/day12.cpp
@@ -2,6 +2,7 @@
 #include "parsing.h"
 
 #include <span>
+#include <string>
 
 namespace Solution {
 
@@ -12,20 +13,65 @@ namespace Solution {
 #define FILE_PATH ".\\inputs\\day12.txt"
 #endif // ------------------------------------
 
-std::string part1() {
+// Row read from the input
+struct Row {
+    char *status;
+    std::vector<u8> groups;
+    usize num = 0; // To be incremented when a new valid arrangement is found
+};
+void debug_rows(std::span<Row> rows) {
+    for (Row &r : rows) {
+        printf("[%lld] %s : ", r.num, r.status);
+        for (u8 g : r.groups) {
+            printf("%i ", g);
+        }
+        printf("\n");
+    }
+}
+
+// Row that is currently being iterated on in the BFS algo
+struct RowState {
+};
+
+std::string part1(std::span<Row> rows) {
+    Row &r = rows[0]; {
+    // for (Row &r : rows)
+        std::vector<RowState> stack;
+        while (!stack.empty()) {
+            // Get status with replacements
+            // If no more ?'s, (NEED TO VALIDATE ANYWAY) row.num++
+            // Else check next ?
+            // If both '.' and '#' are possible, push both states
+            // Else only add possible state
+        }
+    }
+
+    usize total = 0;
+    for (Row &r : rows) {
+        total += r.num;
+    }
+    return std::to_string(total);
+}
+
+std::string part2(std::span<Row> rows) {
     return "NotCompleted";
 }
 
-std::string part2() {
-    return "NotCompleted";
-}
-
-int run(std::string *part1_out, std::string *part2_out) {
+i32 run(std::string *part1_out, std::string *part2_out) {
     std::string in = INCLUDE_STR(FILE_PATH);
-    printf("%s\n", in.c_str());
+    std::vector<Row> rows;
+    Parse::enum_str(std::move(in), "\n", [&rows](char *token){
+        std::vector<char*> sections = Parse::split_char(token, " ");
+        std::vector<char*> groupStr = Parse::split_char(sections[1], ",");
+        std::vector<u8> groups;
+        for (char *c : groupStr) {
+            groups.push_back(std::stoi(c));
+        }
+        rows.push_back({ sections[0], groups });
+    });
 
-    *part1_out = part1();
-    *part2_out = part2();
+    *part1_out = part1(rows);
+    *part2_out = part2(rows);
 
     return 0;
 }
