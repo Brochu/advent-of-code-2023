a44b1bad8b4d5b9a2c98207414d49983bda7a542
Brochu <alexbrochu1@gmail.com>
2023-12-13 00:46:58 -0500

    [DEV] Day13: Prepared for next day, still need some work on day12

---
+++ src/day13.cpp
@@ -0,0 +1,33 @@
+#include "day.h"
+#include "parsing.h"
+
+#include <span>
+
+namespace Solution {
+
+#define DEMO 1
+#if DEMO == 1 // ------------------------------------
+#define FILE_PATH ".\\inputs\\day13_demo1.txt"
+#else // ------------------------------------
+#define FILE_PATH ".\\inputs\\day13.txt"
+#endif // ------------------------------------
+
+std::string part1() {
+    return "NotCompleted";
+}
+
+std::string part2() {
+    return "NotCompleted";
+}
+
+int run(std::string *part1_out, std::string *part2_out) {
+    std::string in = INCLUDE_STR(FILE_PATH);
+    printf("%s\n", in.c_str());
+
+    *part1_out = part1();
+    *part2_out = part2();
+
+    return 0;
+}
+
+}
