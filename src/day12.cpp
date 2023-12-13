14824b734818ef93a5132666d1409189cc680509
Brochu <alexbrochu1@gmail.com>
2023-12-12 23:25:26 -0500

    [DEV] Day12: Optimized unused lines

--- src/day12.cpp
+++ src/day12.cpp
@@ -5,7 +5,7 @@
 
 namespace Solution {
 
-#define DEMO 0
+#define DEMO 1
 #if DEMO == 1 // ------------------------------------
 #define FILE_PATH ".\\inputs\\day12_demo1.txt"
 #else // ------------------------------------
@@ -15,56 +15,40 @@ namespace Solution {
 struct Row {
     char *status;
     std::vector<u8> groups;
-    usize missCount;
     usize num = 0; // To be incremented when a new valid arrangement is found
 };
-void debug_rows(std::span<Row> rows) {
-    for (Row &r : rows) {
-        printf("[%lld][%lld ?s] %s : ", r.num, r.missCount, r.status);
-        for (u8 g : r.groups) {
-            printf("%i ", g);
-        }
-        printf("\n");
-    }
-}
 
 struct RowState {
-    char springs[64];
     usize idx;
     usize len;
+    char springs[64];
 };
 RowState new_state(Row & row) {
-    RowState state;
+    RowState state { 0, strlen(row.status) };
     memcpy(state.springs, row.status, strlen(row.status));
     state.springs[strlen(row.status)] = '\0';
-    state.idx = 0;
-    state.len = strlen(row.status);
     return state;
 }
 RowState duplicate(RowState &rs, char spring) {
-    //printf("[FROM] %s -> ", rs.springs);
-    RowState newstate;
+    RowState newstate { rs.idx + 1, rs.len };
     memcpy(newstate.springs, rs.springs, rs.len);
-    newstate.springs[rs.len] = '\0';
     newstate.springs[rs.idx] = spring;
-    newstate.idx = rs.idx + 1;
-    newstate.len = rs.len;
-    //printf("[TO] %s\n", newstate.springs);
+    newstate.springs[rs.len] = '\0';
     return newstate;
 }
 bool is_done(RowState &rs) {
     usize questionNum = 0;
-    for (int j = 0; j < rs.len; j++) {
+    for (i32 j = 0; j < rs.len; j++) {
         if (rs.springs[j] == '?') questionNum++;
     }
     return questionNum <= 0;
 }
 
 bool is_valid(Row &r, RowState &rs) {
-    printf("[PERMUTATION]: %s\n", rs.springs);
+    //printf("[PERMUTATION]: %s\n", rs.springs);
     std::vector<u8> validGroups;
     u8 *current = nullptr;
-    for (int i = 0; i < rs.len; i++) {
+    for (i32 i = 0; i < rs.len; i++) {
         if (rs.springs[i] == '#') {
             if (current == nullptr) {
                 validGroups.push_back(0);
@@ -78,7 +62,7 @@ bool is_valid(Row &r, RowState &rs) {
     }
 
     if (r.groups.size() == validGroups.size()) {
-        for (int i = 0; i < validGroups.size(); i++) {
+        for (i32 i = 0; i < validGroups.size(); i++) {
             if (r.groups[i] != validGroups[i]) return false;
         }
         return true;
@@ -87,8 +71,9 @@ bool is_valid(Row &r, RowState &rs) {
 }
 
 std::string part1(std::span<Row> rows) {
-    for (Row &r : rows) {
-        //debug_rows({ &r, 1 });
+    for (i32 i = 0; i < rows.size(); i++) {
+        Row &r = rows[i];
+        //printf("[NEW ROW] idx = %i\n", i);
         std::vector<RowState> stack;
         stack.insert(std::begin(stack), new_state(r));
 
@@ -96,10 +81,7 @@ std::string part1(std::span<Row> rows) {
             RowState rs = stack.back();
             stack.pop_back();
             if (is_done(rs)) {
-                if (is_valid(r, rs)) {
-                    //printf("%s\n", rs.springs);
-                    r.num++;
-                }
+                if (is_valid(r, rs)) { r.num++; }
                 continue;
             }
 
@@ -110,21 +92,11 @@ std::string part1(std::span<Row> rows) {
                 rs.idx++;
                 stack.push_back(rs);
             }
-
-            //char transformed[strlen(rs.ref->status) + 1];
-            //strcpy_s(transformed, strlen(rs.ref->status) + 1, rs.ref->status);
-
-            // Get status with replacements
-            // If no more ?'s, (NEED TO VALIDATE ANYWAY) row.num++
-            // Else check next ?
-            // If both '.' and '#' are possible, push both states
-            // Else only add possible state
         }
     }
 
     usize total = 0;
     for (Row &r : rows) {
-        printf("[NUM] %lld\n", r.num);
         total += r.num;
     }
     return std::to_string(total);
@@ -139,17 +111,12 @@ i32 run(std::string *part1_out, std::string *part2_out) {
     std::vector<Row> rows;
     Parse::enum_str(std::move(in), "\n", [&rows](char *token){
         std::vector<char*> sections = Parse::split_char(token, " ");
-        usize missCount = 0;
-        for (int i = 0; i < strlen(sections[0]); i++) {
-            if (sections[0][i] == '?') missCount++;
-        }
-
         std::vector<char*> groupStr = Parse::split_char(sections[1], ",");
         std::vector<u8> groups;
         for (char *c : groupStr) {
             groups.push_back(std::stoi(c));
         }
-        rows.push_back({ sections[0], groups, missCount });
+        rows.push_back({ sections[0], groups });
     });
 
     *part1_out = part1(rows);
