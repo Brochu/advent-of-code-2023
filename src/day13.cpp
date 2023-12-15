05b15a54da227b98d987df1acb76cb12e5b02f56
Brochu <alexbrochu1@gmail.com>
2023-12-15 17:18:13 -0500

    [DEV] Day13: Parsing seems complete, time to find reflection position

--- src/day13.cpp
+++ src/day13.cpp
@@ -12,20 +12,45 @@ namespace Solution {
 #define FILE_PATH ".\\inputs\\day13.txt"
 #endif // ------------------------------------
 
-std::string part1() {
+struct Pattern {
+    std::string data;
+    usize height;
+    usize width;
+};
+
+std::string part1(std::span<Pattern> pats) {
+    //Pattern &p = pats[0];
+    for (Pattern &p : pats) {
+        for (usize i = 0; i < p.height; i++) {
+            for (usize j = 0; j < p.width; j++) {
+                printf("%c", p.data[j + (i * p.width)]);
+            }
+            printf("\n");
+        }
+        printf("------------\n");
+    }
+
     return "NotCompleted";
 }
 
-std::string part2() {
+std::string part2(std::span<Pattern> pats) {
     return "NotCompleted";
 }
 
 int run(std::string *part1_out, std::string *part2_out) {
-    std::string in = INCLUDE_STR(FILE_PATH);
-    printf("%s\n", in.c_str());
+    std::vector<Pattern> patterns;
 
-    *part1_out = part1();
-    *part2_out = part2();
+    std::string in = INCLUDE_STR(FILE_PATH);
+    std::vector<char*> blocks = Parse::split_str(std::move(in), "\n\n");
+    for (char *b : blocks) {
+        std::vector<char*> lines = Parse::split_char(b, "\n");
+        Pattern p { {}, lines.size(), strlen(lines[0]) };
+        for (char *l : lines) { p.data.append(l); }
+        patterns.push_back(p);
+    }
+
+    *part1_out = part1(patterns);
+    *part2_out = part2(patterns);
 
     return 0;
 }
