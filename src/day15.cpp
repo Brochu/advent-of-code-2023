e1b17eb03879ca772ba2246db000733cab4371ea
Brochu <alexbrochu1@gmail.com>
2023-12-16 00:06:09 -0500

    [DEV] Day15: hash for part 2 seems to work, need to calc points

--- src/day15.cpp
+++ src/day15.cpp
@@ -5,12 +5,17 @@
 
 namespace Solution {
 
-#define DEMO 0
+#define DEMO 1
 #if DEMO == 1 // ------------------------------------
 #define FILE_PATH ".\\inputs\\day15_demo1.txt"
 #else // ------------------------------------
 #define FILE_PATH ".\\inputs\\day15.txt"
 #endif // ------------------------------------
+
+struct Lens {
+    char label[256];
+    u8 focus;
+};
 
 usize hash_algo(const char *word) {
     u64 value = 0;
@@ -36,7 +41,55 @@ std::string part1(std::span<char*> words) {
 }
 
 std::string part2(std::span<char*> words) {
-    return "NotCompleted";
+    std::vector<Lens> boxes[256];
+    usize result = 0;
+
+    //char *word = words[0]; {
+    for (char *word : words) {
+        usize count = 0;
+        while (word[count] != '=' && word[count] != '-') { count++; }
+        Lens l;
+        memcpy(l.label, word, count);
+        l.label[count] = '\0';
+        usize idx = hash_algo(l.label);
+
+        char op = word[count];
+        if (word[count] == '=') {
+            l.focus = word[count + 1] - '0';
+            printf("[ADD] label: %s (%lld), focus: %i\n", l.label, idx, l.focus);
+            std::vector<Lens> &box = boxes[idx];
+            auto pos = std::find_if(box.begin(), box.end(), [&l](const Lens &in){
+                return strcmp(in.label, l.label) == 0;
+            });
+            if (pos == box.end()) {
+                box.insert(box.end(), l);
+            } else {
+                (*pos).focus = l.focus;
+            }
+        }
+        else if (word[count] == '-') {
+            printf("[DEL] label: %s (%lld)\n", l.label, idx);
+            std::vector<Lens> &box = boxes[idx];
+            auto pos = std::find_if(box.begin(), box.end(), [&l](const Lens &in){
+                return strcmp(in.label, l.label) == 0;
+            });
+            if (pos != box.end()) {
+                box.erase(pos);
+            }
+        }
+    }
+
+    for (i32 i = 0; i < 256; i++) {
+        if (boxes[i].size() > 0) {
+            printf("[BOX %i] > ", i);
+            for (Lens &l : boxes[i]) {
+                printf("(%s, %i) ", l.label, l.focus);
+            }
+            printf("\n");
+        }
+    }
+
+    return std::to_string(result);
 }
 
 int run(std::string *part1_out, std::string *part2_out) {
