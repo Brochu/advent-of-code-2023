40cf67d8310eb1b56253bddd0045deda91b6cc94
Brochu <alexbrochu1@gmail.com>
2023-12-17 02:48:22 -0500

    [DEV] Day17: Parsing the map should be done
    Next is going to be a modified Dijkstra algo with some extra
    restrictions

--- src/day17.cpp
+++ src/day17.cpp
@@ -1,7 +1,9 @@
 #include "day.h"
 #include "parsing.h"
 
+#include <queue>
 #include <span>
+#include <unordered_map>
 
 namespace Solution {
 
@@ -12,8 +14,25 @@ namespace Solution {
 #define FILE_PATH ".\\inputs\\day17.txt"
 #endif // ------------------------------------
 
-std::string part1() {
-    return "NotCompleted";
+struct Map {
+    u8 *cells;
+    usize h;
+    usize w;
+};
+void debug_map(Map &map) {
+    for (i32 i = 0; i < map.h; i++) {
+        for (i32 j = 0; j < map.w; j++) {
+            printf("%i ", map.cells[j + (i * map.w)]);
+        }
+        printf("\n");
+    }
+}
+
+std::string part1(Map &map) {
+    debug_map(map);
+
+    std::unordered_map<usize, u32> dists;
+    return std::to_string(dists[(map.h * map.w) - 1]);
 }
 
 std::string part2() {
@@ -22,11 +41,19 @@ std::string part2() {
 
 int run(std::string *part1_out, std::string *part2_out) {
     std::string in = INCLUDE_STR(FILE_PATH);
-    printf("%s\n", in.c_str());
+    std::vector<char*> lines = Parse::split_str(std::move(in), "\n");
 
-    *part1_out = part1();
+    Map m { new u8[lines.size() * strlen(lines[0])](), lines.size(), strlen(lines[0]) };
+    for (i32 i = 0; i < m.h; i++) {
+        for (i32 j = 0; j < m.w; j++) {
+            m.cells[j + (i * m.w)] = lines[i][j] - '0';
+        }
+    }
+
+    *part1_out = part1(m);
     *part2_out = part2();
 
+    delete[] m.cells;
     return 0;
 }
 
