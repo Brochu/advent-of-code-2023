5ee22613ad44c7c81c9e7bca42e255beff96b37f
Brochu <alexbrochu1@gmail.com>
2023-12-18 02:07:42 -0500

    [NOTES] Day17: Found the issue, some things wrong with algo

--- src/day17.cpp
+++ src/day17.cpp
@@ -74,8 +74,21 @@ struct State {
     u32 count;
 };
 
+Direction get_dir(i32 x, i32 y, i32 nx, i32 ny) {
+    if (x - nx == 1) return Direction::LEFT;
+    else if (x - nx == -1) return Direction::RIGHT;
+    else if (y - ny == 1) return Direction::UP;
+    else if (y - ny == -1) return Direction::DOWN;
+    else return Direction::NONE;
+}
+
 std::string part1(Map &map) {
     debug_map(map);
+
+    auto cmp = [](State &left, State &right){ return left.dist > right.dist; };
+    std::priority_queue<State, std::vector<State>, decltype(cmp)> Q(cmp);
+    Q.push({ 0, 0, 0.f, NONE, 1 }); //THIS IS WRONG
+    //TODO: We need to store same dir jump counts in prev?
 
     std::unordered_map<usize, usize> prevs;
     std::unordered_map<usize, float> dists;
@@ -84,14 +97,11 @@ std::string part1(Map &map) {
             const usize offset = j + (i * map.w);
             prevs[offset] = ~0;
             dists[offset] = std::numeric_limits<float>::infinity();
+            Q.push({ j, i, std::numeric_limits<float>::infinity(), NONE, 1 });
         }
     }
     prevs[0] = ~0;
     dists[0] = 0.f;
-
-    auto cmp = [](State &left, State &right){ return left.dist > right.dist; };
-    std::priority_queue<State, std::vector<State>, decltype(cmp)> Q(cmp);
-    Q.push({ 0, 0, 0.f, NONE, 1 });
 
     while (!Q.empty()) {
         const State u = Q.top();
@@ -99,39 +109,35 @@ std::string part1(Map &map) {
         if (u.count > 3) continue;
 
         usize idx = u.x + (u.y * map.w);
-        //printf("[PICKED] (%i, %i) (from %i; %i) [%f]\n", u.x, u.y, u.from, u.count, u.dist);
+        printf("[PICKED] (%i, %i) (from %i; %i) [%f]\n", u.x, u.y, u.from, u.count, u.dist);
 
         std::vector<usize> neighbors;
-        if (u.y > 0 && u.from != Direction::UP) {
+        if (u.y > 0) {
             neighbors.push_back(u.x + ((u.y - 1) * map.w));
         }
-        if (u.y < (map.h - 1) && u.from != Direction::DOWN) {
+        if (u.y < (map.h - 1)) {
             neighbors.push_back(u.x + ((u.y + 1) * map.w));
         }
-        if (u.x > 0 && u.from != Direction::LEFT) {
+        if (u.x > 0) {
             neighbors.push_back((u.x - 1) + (u.y * map.w));
         }
         if (u.x < (map.w - 1) && u.from != Direction::RIGHT) {
             neighbors.push_back((u.x + 1) + (u.y * map.w));
         }
         for (usize n : neighbors) {
-            if (dists[idx] + map.cells[n] <= dists[n]) {
+            if (dists[idx] + map.cells[n] < dists[n]) {
                 prevs[n] = idx;
                 dists[n] = dists[idx] + map.cells[n];
                 i32 x = n % map.w;
                 i32 y = n / map.w;
-                Direction from = NONE;
-                if (x - u.x == 1) from = Direction::LEFT;
-                else if (x - u.x == -1) from = Direction::RIGHT;
-                else if (y - u.y == 1) from = Direction::UP;
-                else if (y - u.y == -1) from = Direction::DOWN;
-                //printf("[ADDED] (%i, %i), prev = (%i, %i), dist = %f\n", x, y, u.x, u.y, dists[n]);
+                printf("[ADDED] (%i, %i), prev = (%i, %i), dist = %f\n", x, y, u.x, u.y, dists[n]);
+                Direction from = get_dir(x, y, u.x, u.y);
                 Q.push({ x, y, dists[n], from, u.from == from ? u.count + 1 : 1 });
             }
         }
         //std::cin.ignore(1);
     }
-    //debug_dists(map, dists);
+    debug_dists(map, dists);
     debug_path(map, prevs);
 
     const usize exit = (map.h * map.w) - 1;
