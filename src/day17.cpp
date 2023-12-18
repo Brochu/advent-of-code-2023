6e6d9e507553b5ce0124d45d593635862645ea62
Brochu <alexbrochu1@gmail.com>
2023-12-17 19:09:47 -0500

    [DEV] Day17: Path finding seems to work, doesn't track same dir moving

--- src/day17.cpp
+++ src/day17.cpp
@@ -15,6 +15,7 @@ namespace Solution {
 #define FILE_PATH ".\\inputs\\day17.txt"
 #endif // ------------------------------------
 
+enum Direction { NONE, UP, LEFT, RIGHT, DOWN };
 struct Map {
     u8 *cells;
     usize h;
@@ -28,11 +29,32 @@ void debug_map(Map &map) {
         printf("\n");
     }
 }
+void debug_path(Map &map, std::unordered_map<usize, usize> &prevs) {
+    usize offset = (map.w * map.h) - 1;
+    printf("[PATH] ");
+    while (prevs[offset] != ~0) {
+        i32 x = prevs[offset] % map.w;
+        i32 y = prevs[offset] / map.w;
+        printf("(%i, %i); ", x, y);
+
+        offset = prevs[offset];
+    }
+    printf("\n");
+}
+void debug_dists(Map &map, std::unordered_map<usize, float> &dists) {
+    for (i32 i = 0; i < map.h; i++) {
+        for (i32 j = 0; j < map.w; j++) {
+            printf("%f ", dists[j + (i * map.w)]);
+        }
+        printf("\n");
+    }
+}
 
 struct State {
     i32 x, y;
-    i32 prevx, prevy;
     float dist;
+    Direction from;
+    u32 count;
 };
 
 std::string part1(Map &map) {
@@ -52,32 +74,47 @@ std::string part1(Map &map) {
 
     auto cmp = [](State &left, State &right){ return left.dist > right.dist; };
     std::priority_queue<State, std::vector<State>, decltype(cmp)> Q(cmp);
-    Q.push({ 0, 0, -1, -1, 0.f });
+    Q.push({ 0, 0, 0.f, NONE, 1 });
 
     while (!Q.empty()) {
         const State u = Q.top();
         Q.pop();
-        printf("[PICKED] (%i, %i) (from %i, %i) [%f]\n", u.x, u.y, u.prevx, u.prevy, u.dist);
+        usize idx = u.x + (u.y * map.w);
+        printf("[PICKED] (%i, %i) (from %i; %i) [%f]\n", u.x, u.y, u.from, u.count, u.dist);
 
         std::vector<usize> neighbors;
-        if (u.y > 0 && (u.x != u.prevx || (u.y - 1) != u.prevy)) {
+        if (u.y > 0 && u.from != Direction::UP) {
             neighbors.push_back(u.x + ((u.y - 1) * map.w));
         }
-        if (u.y < (map.h - 1) && (u.x != u.prevx || (u.y + 1) != u.prevy)) {
+        if (u.y < (map.h - 1) && u.from != Direction::DOWN) {
             neighbors.push_back(u.x + ((u.y + 1) * map.w));
         }
-        if (u.x > 0 && ((u.x - 1) != u.prevx || u.y != u.prevy)) {
+        if (u.x > 0 && u.from != Direction::LEFT) {
             neighbors.push_back((u.x - 1) + (u.y * map.w));
         }
-        if (u.x < (map.w - 1) && ((u.x + 1) != u.prevx || u.y != u.prevy)) {
+        if (u.x < (map.w - 1) && u.from != Direction::RIGHT) {
             neighbors.push_back((u.x + 1) + (u.y * map.w));
         }
-        printf("Neighbors -> \n");
+        //printf("Neighbors -> \n");
         for (usize n : neighbors) {
-            printf("%lld -> (%lld, %lld)\n", n, n % map.w, n / map.w);
+            //printf("%lld -> (%lld, %lld)\n", n, n % map.w, n / map.w);
             //TODO: Condition checks for updating dists and prevs
+            if (dists[idx] + map.cells[n] < dists[n]) {
+                prevs[n] = idx;
+                dists[n] = dists[idx] + map.cells[n];
+                i32 x = n % map.w;
+                i32 y = n / map.w;
+                Direction from = UP;
+                if (x - u.x == 1) from = Direction::LEFT;
+                else if (x - u.x == -1) from = Direction::RIGHT;
+                else if (y - u.y == 1) from = Direction::UP;
+                else if (y - u.y == -1) from = Direction::DOWN;
+                Q.push({ x, y, dists[n], from, u.from == from ? u.count + 1 : 1 });
+            }
         }
     }
+    //debug_dists(map, dists);
+    debug_path(map, prevs);
 
     const usize exit = (map.h * map.w) - 1;
     return std::to_string(dists[exit]);
