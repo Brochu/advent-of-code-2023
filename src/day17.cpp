cd47cbbe3f1ee54f161dc8f2692b97b3bc1f26be
Brochu <alexbrochu1@gmail.com>
2023-12-19 03:23:27 -0500

    [DEV] Day17: New try, the dist and prev maps changed to arrays
    We also duplicate these arrays for each directions

--- src/day17.cpp
+++ src/day17.cpp
@@ -1,11 +1,9 @@
 #include "day.h"
 #include "parsing.h"
 
-#include <iostream>
 #include <limits>
 #include <queue>
 #include <span>
-#include <unordered_map>
 
 namespace Solution {
 
@@ -15,60 +13,20 @@ namespace Solution {
 #else // ------------------------------------
 #define FILE_PATH ".\\inputs\\day17.txt"
 #endif // ------------------------------------
+#define INF std::numeric_limits<float>::infinity()
 
 struct Pos { i32 x, y; };
-Pos* find_pos(std::span<Pos> list, Pos needle) {
-    auto it = std::find_if(list.begin(), list.end(), [&needle](Pos &in){
-        return needle.x == in.x && needle.y == in.y;
-    });
-    if (it != list.end()) { return &*it; }
-    return nullptr;
-}
-void get_dirs(Pos &p, std::span<Pos> dirs) {
-    dirs[0] = { p.y, -p.x };
-    dirs[1] = p;
-    dirs[2] = { -p.y, p.x };
-}
 
 struct Map {
     u8 *cells;
     usize height;
     usize width;
-
-    u8 get(Pos p){ return cells[p.y * width + p.x]; }
-    bool is_valid(Pos p) { return (p.x >= 0 && p.x < width) && (p.y >= 0 && p.y < height); }
 };
-usize pos_offset(Map &map, Pos p) { return p.y * map.width + p.x; }
-Pos offset_pos(Map &map, usize o) {
-    i32 x = o % map.width;
-    i32 y = o / map.width;
-    return { x, y };
-}
 void debug_map(Map &map) {
     for (i32 i = 0; i < map.height; i++) {
-        for (i32 j = 0; j < map.width; j++) { printf("%i ", map.get({ i, j })); }
-        printf("\n");
-    }
-}
-void debug_path(Map &map, std::unordered_map<usize, Pos> &prevs) {
-    std::vector<Pos> path;
-    usize offset = (map.width * map.height) - 1;
-    path.push_back({ (i32)map.width - 1, (i32)map.height - 1 });
-
-    while (prevs[offset].x != -1 && prevs[offset].y != -1) {
-        path.insert(path.begin(), prevs[offset]);
-        offset = pos_offset(map ,prevs[offset]);
-    }
-
-    printf("[PATH]\n");
-    for (Pos &node : path) {
-        printf("(%i, %i)\n", node.x, node.y);
-    }
-    for (i32 i = 0; i < map.height; i++) {
         for (i32 j = 0; j < map.width; j++) {
-            Pos *p = find_pos(path, { i, j });
-            if (p != nullptr) printf("* ");
-            else printf("%i ", map.get({ i, j }));
+            const usize idx = i * map.width + j;
+            printf("%i ", map.cells[idx]);
         }
         printf("\n");
     }
@@ -86,43 +44,30 @@ std::string part1(Map &map) {
 
     auto cmp = [](State &left, State &right){ return left.loss > right.loss; };
     std::priority_queue<State, std::vector<State>, decltype(cmp)> Q(cmp);
-    Q.push({ start, {1, 0}, 0, 0 });
     Q.push({ start, {0, 1}, 0, 0 });
+    Q.push({ start, {1, 0}, 0, 0 });
 
-    std::unordered_map<usize, Pos> prevs;
-    std::unordered_map<usize, float> dists;
+    Pos prev[map.width * map.height][4];
+    float dist[map.width * map.height][4];
     for (i32 i = 0; i < map.height; i++) {
         for (i32 j = 0; j < map.width; j++) {
             const usize offset = i * map.width + j;
-            prevs[offset] = { -1, -1 };
-            dists[offset] = std::numeric_limits<float>::infinity();
+            prev[offset][0] = prev[offset][1] = prev[offset][2] = prev[offset][3] = { -1, -1 };
+            dist[offset][0] = dist[offset][1] = dist[offset][2] = dist[offset][3] = INF;
         }
     }
-    prevs[0] = { -1, -1 };
-    dists[0] = 0.f;
+    prev[0][0] = prev[0][1] = prev[0][2] = prev[0][3] = { -1, -1 };
+    dist[0][0] = dist[0][1] = dist[0][2] = dist[0][3] = 0.f;
 
     while (!Q.empty()) {
         State u = Q.top();
         Q.pop();
-        const usize uo = pos_offset(map, u.pos);
-        debug_state(u);
-
-        printf("[DIRECTIONS]:\n");
-        Pos dirs[3];
-        get_dirs(u.dir, dirs);
-
-        for (Pos dir : dirs) {
-            Pos newpos = { u.pos.x + dir.x, u.pos.y + dir.y };
-            if (map.is_valid(newpos)) {
-                const usize offset = pos_offset(map, newpos);
-                printf("- (%i, %i), (%i, %i) [%lld]\n", dir.x, dir.y, newpos.x, newpos.y, offset);
-            }
-        }
     }
     //debug_path(map, prevs);
 
     const usize exit = (map.width * map.height) - 1;
-    return std::to_string(dists[exit]);
+    const float result = std::min(dist[exit][0], std::min(dist[exit][1], std::min(dist[exit][2], dist[exit][3])));
+    return std::to_string(result);
 }
 
 std::string part2() {
