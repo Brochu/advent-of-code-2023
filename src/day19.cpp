8be187aee63c9c653ffcae56e50cb8c43bd9ef2e
Brochu <alexbrochu1@gmail.com>
2023-12-20 11:08:33 -0500

    [DEV] Day19: Rewrite to see if my first pass' logic was wrong

--- src/day19.cpp
+++ src/day19.cpp
@@ -15,26 +15,35 @@ namespace Solution {
 struct Stage {
     char var;
     char op;
-    int value;
+    u32 value;
     char *target;
 };
 struct Flow {
     const char *name;
     std::vector<Stage> stages;
+    const char *fallback;
 };
 void debug_flow(Flow &flow) {
-    printf("[FLOW] '%s'\n", flow.name);
+    printf("[FLOW] '%s'; fallback='%s'\n", flow.name, flow.fallback);
     for (Stage &s : flow.stages) {
-        if (s.op == '\0') printf(" - -> '%s'\n", s.target);
-        else printf(" - %c %c %i -> '%s'\n", s.var, s.op, s.value, s.target);
+        printf(" - %c %c %i -> '%s'\n", s.var, s.op, s.value, s.target);
     }
+}
+Flow* find_flow(std::span<Flow> flows, const char *needle) {
+    for (Flow &fl : flows) {
+        if (strcmp(fl.name, needle) == 0) { return &fl; }
+    }
+    return nullptr;
 }
 
 struct Part {
-    int values[4]; // x, m, a, s (LOL didn't notice at first)
+    u32 values[4]; // x, m, a, s (LOL didn't notice at first)
 };
 void debug_part(Part &part) {
     printf("[PART] x=%i; m=%i; a=%i; s=%i\n", part.values[0], part.values[1], part.values[2], part.values[3]);
+}
+usize part_value(Part &p) {
+    return p.values[0] + p.values[1] + p.values[2] + p.values[3];
 }
 
 usize var_idx(char var) {
@@ -45,51 +54,37 @@ usize var_idx(char var) {
     else return INT_MAX;
 }
 
-std::string part1(std::span<Flow> flows, std::span<Part> parts) {
-    auto pred = [](Flow &f){ return strcmp(f.name, "in") == 0; };
-    //debug_flow(*in);
+const char* apply_flow(Flow &fl, Part &p) {
+    for (Stage &st : fl.stages) {
+        u32 part_val = p.values[var_idx(st.var)];
 
-    usize count = 0;
-    //Part &p = parts[0]; {
-    for (Part &p : parts) {
-        debug_part(p);
-        auto ongoing = std::find_if(flows.begin(), flows.end(), pred);
-        while (strcmp(ongoing->name, "A") != 0 && strcmp(ongoing->name, "R") != 0) {
-            //debug_flow(*ongoing);
-            const char *final = "";
-            for (int i = 0; i < ongoing->stages.size() - 1; i++) {
-                Stage &s = ongoing->stages[i];
-                int part_val = p.values[var_idx(s.var)];
-
-                bool check = false;
-                if (s.op == '<') check = part_val < s.value;
-                else if (s.op == '>') check = part_val > s.value;
-
-                if (check) {
-                    final = s.target;
-                }
-            }
-
-            if (strcmp(final, "") == 0) {
-                final = ongoing->stages[ongoing->stages.size() - 1].target;
-            }
-
-            ongoing = std::find_if(flows.begin(), flows.end(), [&final](Flow &f){
-                return strcmp(f.name, final) == 0;
-            });
+        if (st.op == '<' && part_val < st.value) {
+            return st.target;
         }
-
-        debug_flow(*ongoing);
-        printf("\n");
-        if (strcmp(ongoing->name, "A") == 0) {
-            count += p.values[0];
-            count += p.values[1];
-            count += p.values[2];
-            count += p.values[3];
+        else if (st.op == '>' && part_val > st.value) {
+            return st.target;
         }
     }
+    return fl.fallback;
+}
 
-    return std::to_string(count);
+std::string part1(std::span<Flow> flows, std::span<Part> parts) {
+    usize rating = 0;
+
+    Part part = parts[0];
+    const char *curr = "in";
+    while (strcmp(curr, "A") != 0 && strcmp(curr, "R") != 0) {
+        Flow *fl = find_flow(flows, curr);
+        debug_flow(*fl);
+
+        curr = apply_flow(*fl, part);
+        printf("next: '%s'\n", curr);
+    }
+
+    if (strcmp(curr, "A") == 0) {
+        rating += part_value(part);
+    }
+    return std::to_string(rating);
 }
 
 std::string part2() {
@@ -123,13 +118,11 @@ int run(std::string *part1_out, std::string *part2_out) {
                 s.target = elems[1];
                 fl.stages.push_back(s);
             } else {
-                fl.stages.push_back({ '\0', '\0', INT_MAX, stage });
+                fl.fallback = stage;
             }
         }
         workflows.push_back(fl);
     });
-    workflows.push_back({ "A" });
-    workflows.push_back({ "R" });
 
     std::vector<Part>parts;
     Parse::enum_char(sections[1], "\n", [&parts](char *token){
@@ -140,7 +133,7 @@ int run(std::string *part1_out, std::string *part2_out) {
 
         Part p;
         std::vector<char*> vals = Parse::split_char(token, ",");
-        for (int i = 0; i < vals.size(); i++) {
+        for (u32 i = 0; i < vals.size(); i++) {
             vals[i] += 2;
             p.values[i] = std::stoi(vals[i]);
         }
