d2d6658acebd1653df0ed509592b8f7c655ee2a8
Brochu <alexbrochu1@gmail.com>
2023-12-23 00:54:08 -0500

    [DEV] Day23: sick parsing trick with enums

--- src/day23.cpp
+++ src/day23.cpp
@@ -14,7 +14,29 @@ namespace Solution {
 #define MAP_SIZE 141
 #endif // ------------------------------------
 
-usize part1() {
+enum Cell {
+    Path = '.',
+    Tree = '#',
+    Up   = '^',
+    Left = '<',
+    Right= '>',
+    Down = 'v',
+};
+struct Map {
+    std::vector<Cell> cells;
+};
+void debug_map(Map &map) {
+    for (i32 i = 0; i < MAP_SIZE; i++) {
+        for (i32 j = 0; j < MAP_SIZE; j++) {
+            const usize offset = i * MAP_SIZE + j;
+            printf("%c", map.cells[offset]);
+        }
+        printf("\n");
+    }
+}
+
+usize part1(Map &map) {
+    debug_map(map);
     return 0;
 }
 
@@ -24,9 +46,15 @@ usize part2() {
 
 int run(std::string *part1_out, std::string *part2_out) {
     std::string in = INCLUDE_STR(FILE_PATH);
-    printf("%s", in.c_str());
+    Map map;
+    std::vector<char*> lines = Parse::split_str(std::move(in), "\n");
+    for (i32 i = 0; i < lines.size(); i++) {
+        for (i32 j = 0; j < strlen(lines[0]); j++) {
+            map.cells.push_back((Cell)lines[i][j]);
+        }
+    }
 
-    *part1_out = std::to_string(part1());
+    *part1_out = std::to_string(part1(map));
     *part2_out = std::to_string(part2());
 
     return 0;
