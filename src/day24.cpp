4b90b1224353891f92813c1359ab8ada9173f338
Brochu <alexbrochu1@gmail.com>
2023-12-24 01:52:44 -0500

    [DEV] Day24: Solved for part 1, equation solving to find intersections

--- src/day24.cpp
+++ src/day24.cpp
@@ -5,11 +5,15 @@
 
 namespace Solution {
 
-#define DEMO 1
+#define DEMO 0
 #if DEMO == 1 // ------------------------------------
 #define FILE_PATH ".\\inputs\\day24_demo1.txt"
+#define MIN_COORD 7
+#define MAX_COORD 27
 #else // ------------------------------------
 #define FILE_PATH ".\\inputs\\day24.txt"
+#define MIN_COORD 200000000000000
+#define MAX_COORD 400000000000000
 #endif // ------------------------------------
 
 struct Vec3 {
@@ -34,10 +38,42 @@ void debug_line(Line &line) {
 }
 
 usize part1(std::span<Line> hail) {
-    for (Line &h : hail) {
-        debug_line(h);
+    usize count = 0;
+
+    //i32 i = 0; {
+    for (i32 i = 0; i < hail.size(); i++) {
+        //i32 j = 1; {
+        for (i32 j = i; j < hail.size(); j++) {
+            if (j == i) continue;
+
+            Line &l0 = hail[i];
+            const double m0 = (double)l0.slope.y / l0.slope.x;
+            const double b0 = l0.sample.y - (m0 * l0.sample.x);
+            //printf("[LINE 0] m=%f, b=%f\n", m0, b0);
+            Line &l1 = hail[j];
+            const double m1 = (double)l1.slope.y / l1.slope.x;
+            const double b1 = l1.sample.y - (m1 * l1.sample.x);
+            //printf("[LINE 1] m=%f, b=%f\n", m1, b1);
+
+            if (m0 - m1 == 0) {
+                //printf("[CROSS] INVALID, Won't cross...\n");
+                continue;
+            }
+            const double x = (b1 - b0) / (m0 - m1);
+            const double y = (m0 * x) + b0;
+            //printf("[CROSS] x=%f; y=%f\n", x, y);
+            //printf("[LIMITS] MIN=%i; MAX=%i\n", MIN_COORD, MAX_COORD);
+
+            bool in_range = x >= MIN_COORD && x <= MAX_COORD && y >= MIN_COORD && y <= MAX_COORD;
+            bool in_apast = l0.slope.x < 0 ? x > l0.sample.x : x < l0.sample.x;
+            bool in_bpast = l1.slope.x < 0 ? x > l1.sample.x : x < l1.sample.x;
+            //printf("[COND] in_range=%i; apast=%i; bpast=%i\n", in_range, in_apast, in_bpast);
+            if (in_range && !in_apast && !in_bpast) {
+                count++;
+            }
+        }
     }
-    return 0;
+    return count;
 }
 
 usize part2() {
