e133b8f85eb9ef6284046e463e99838d3481404b
Brochu <alexbrochu1@gmail.com>
2023-12-24 00:53:36 -0500

    [DEV] Day24: Input parsing done

--- src/day24.cpp
+++ src/day24.cpp
@@ -12,7 +12,31 @@ namespace Solution {
 #define FILE_PATH ".\\inputs\\day24.txt"
 #endif // ------------------------------------
 
-usize part1() {
+struct Vec3 {
+    double x;
+    double y;
+    double z;
+};
+void debug_vec3(Vec3 &vec) {
+    printf("(%f, %f, %f)", vec.x, vec.y, vec.z);
+}
+
+struct Line {
+    Vec3 sample;
+    Vec3 slope;
+};
+void debug_line(Line &line) {
+    printf("[LINE]: sample=");
+    debug_vec3(line.sample);
+    printf("; slope=");
+    debug_vec3(line.slope);
+    printf("\n");
+}
+
+usize part1(std::span<Line> hail) {
+    for (Line &h : hail) {
+        debug_line(h);
+    }
     return 0;
 }
 
@@ -22,9 +46,16 @@ usize part2() {
 
 int run(std::string *part1_out, std::string *part2_out) {
     std::string in = INCLUDE_STR(FILE_PATH);
-    printf("%s", in.c_str());
+    std::vector<Line> hail;
+    Parse::enum_str(std::move(in), "\n", [&hail](char *line){
+        i64 px, py, pz;
+        i64 vx, vy, vz;
+        //TODO: Check why I cannot just read doubles here???
+        sscanf_s(line, "%lld, %lld, %lld @ %lld, %lld, %lld", &px, &py, &pz, &vx, &vy, &vz);
+        hail.push_back({ {(double)px, (double)py, (double)pz}, {(double)vx, (double)vy, (double)vz} });
+    });
 
-    *part1_out = std::to_string(part1());
+    *part1_out = std::to_string(part1(hail));
     *part2_out = std::to_string(part2());
 
     return 0;
