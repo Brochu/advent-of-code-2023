927cf0dd6508d50fea564f4531b62424372961e8
Brochu <alexbrochu1@gmail.com>
2023-12-23 23:13:32 -0500

    [DEV] Day24: Prepared for second to last challenge of this year

---
+++ src/day24.cpp
@@ -0,0 +1,33 @@
+#include "day.h"
+#include "parsing.h"
+
+#include <span>
+
+namespace Solution {
+
+#define DEMO 1
+#if DEMO == 1 // ------------------------------------
+#define FILE_PATH ".\\inputs\\day24_demo1.txt"
+#else // ------------------------------------
+#define FILE_PATH ".\\inputs\\day24.txt"
+#endif // ------------------------------------
+
+usize part1() {
+    return 0;
+}
+
+usize part2() {
+    return 0;
+}
+
+int run(std::string *part1_out, std::string *part2_out) {
+    std::string in = INCLUDE_STR(FILE_PATH);
+    printf("%s", in.c_str());
+
+    *part1_out = std::to_string(part1());
+    *part2_out = std::to_string(part2());
+
+    return 0;
+}
+
+}
