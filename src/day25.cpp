1794646f50526026ff0d6c26f11132f29a9da4cf
Brochu <alexbrochu1@gmail.com>
2023-12-25 14:31:55 -0500

    [DEV] Day25: Second pass for parsing

--- src/day25.cpp
+++ src/day25.cpp
@@ -12,16 +12,17 @@ namespace Solution {
 #define FILE_PATH ".\\inputs\\day25.txt"
 #endif // ------------------------------------
 
-struct Component {
-    char *name;
-    std::vector<char*> conns;
-};
 struct Machine {
-    std::vector<Component> cmps;
-    bool **mat;
+    std::vector<char*> names;
+    bool **mat; //TODO: Fill this based off names idx
 };
 
 usize part1(Machine &m) {
+    for (char *name : m.names) {
+        printf(" - '%s'\n", name);
+    }
+    //auto it = std::find(m.names.begin(), m.names.end(), "lsr");
+    //printf(" Found 'lsr' at %s\n", *it);
     return 0;
 }
 
@@ -31,12 +32,27 @@ usize part2(Machine &m) {
 
 int run(std::string *part1_out, std::string *part2_out) {
     std::string in = INCLUDE_STR(FILE_PATH);
+    usize next_idx = 0;
     Machine m;
-    Parse::enum_str(std::move(in), "\n", [&m](char *token){
+    Parse::enum_str(std::move(in), "\n", [&m, &next_idx](char *token){
         char *name = nullptr;
         char *rest = nullptr;
         Parse::split_once(token, ": ", &name, &rest);
-        m.cmps.push_back({ name, Parse::split_char(rest, " ") });
+
+        auto add = [&m](char *name){
+            auto it = std::find_if(m.names.begin(), m.names.end(), [&name](char *in){
+                return strcmp(name, in) == 0;
+            });
+            if (it == m.names.end()) {
+                m.names.push_back(name);
+            }
+        };
+
+        add(name);
+        std::vector<char*> conns = Parse::split_char(rest, " ");
+        for (char *connection : Parse::split_char(rest, " ")) {
+            add(connection);
+        }
     });
 
     *part1_out = std::to_string(part1(m));
