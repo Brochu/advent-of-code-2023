ee00c1ca94ea3ab2e9409de9fb0579a3d7275cd0
Brochu <alexbrochu1@gmail.com>
2023-12-31 00:48:01 -0500

    [CLEAN] Day14: cleaned up logging for part 1

--- src/day14.cpp
+++ src/day14.cpp
@@ -30,20 +30,13 @@ std::string part1(std::span<std::string> lines) {
             usize pos = lines.size() - i;
 
             if (lines[i][j] == 'O') {
-                //printf("Found O at %i\n", j);
                 total += heightmap[j];
                 heightmap[j]--;
             }
             else if (lines[i][j] == '#') {
-                //printf("Found # at %i\n", j);
                 heightmap[j] = pos - 1;
             }
         }
-        //printf("[HEIGHTS]: ");
-        //for (i32 i = 0; i < strlen(lines[0]); i++) {
-        //    printf("%lld, ", heightmap[i]);
-        //}
-        //printf("\n");
     }
     return std::to_string(total);
 }
@@ -64,17 +57,13 @@ void tilt(std::vector<std::string> &lines) {
     for (i32 i = 1; i < lines.size(); i++) {
         for (i32 j = 0; j < lines[i].size(); j++) {
             if (lines[i][j] == 'O') {
-                //printf("(%i, %i)\n", i, j);
                 for (i32 k = i - 1; k >= 0; k--) {
-                    //printf("k=%i\n", k);
                     if (lines[k][j] == 'O' || lines[k][j] == '#') {
-                        //printf("[SWAP] k=%i; i=%i\n", k + 1, i);
                         lines[i][j] = '.';
                         lines[k + 1][j] = 'O';
                         break;
                     }
                     if (k == 0) {
-                        //printf("[SWAP] To start\n");
                         lines[i][j] = '.';
                         lines[0][j] = 'O';
                     }
